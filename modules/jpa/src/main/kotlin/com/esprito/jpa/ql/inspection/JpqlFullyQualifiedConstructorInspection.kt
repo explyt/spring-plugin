d7bb9e418f51a82a41c16d9777f02f2a55a18e7a
Yuriy Burka <bu@esprito.com>
2024-02-01 10:46:04 +0000

    #161 Not only jpa entities
    MR: development/esprito-studio!149

--- modules/jpa/src/main/kotlin/com/esprito/jpa/ql/inspection/JpqlFullyQualifiedConstructorInspection.kt
+++ modules/jpa/src/main/kotlin/com/esprito/jpa/ql/inspection/JpqlFullyQualifiedConstructorInspection.kt
@@ -3,14 +3,13 @@ package com.esprito.jpa.ql.inspection
 import com.esprito.jpa.JpaBundle
 import com.esprito.jpa.ql.psi.JpqlFullyQualifiedConstructor
 import com.esprito.jpa.ql.psi.JpqlVisitor
-import com.esprito.jpa.service.JpaEntitySearch
 import com.intellij.codeInspection.LocalInspectionTool
 import com.intellij.codeInspection.LocalInspectionToolSession
 import com.intellij.codeInspection.ProblemHighlightType
 import com.intellij.codeInspection.ProblemsHolder
-import com.intellij.openapi.module.ModuleUtilCore
-import com.intellij.psi.PsiClass
+import com.intellij.psi.JavaPsiFacade
 import com.intellij.psi.PsiElementVisitor
+import com.intellij.psi.search.GlobalSearchScope
 
 class JpqlFullyQualifiedConstructorInspection : LocalInspectionTool() {
 
@@ -18,30 +17,26 @@ class JpqlFullyQualifiedConstructorInspection : LocalInspectionTool() {
         holder: ProblemsHolder, isOnTheFly: Boolean, session: LocalInspectionToolSession
     ): PsiElementVisitor = object : JpqlVisitor() {
         override fun visitFullyQualifiedConstructor(fqConstructor: JpqlFullyQualifiedConstructor) {
-            val module = ModuleUtilCore.findModuleForPsiElement(fqConstructor) ?: return
             val fqConstructorText = fqConstructor.text
 
-            for (entity in JpaEntitySearch.getInstance(holder.project).loadEntities(module)) {
-                if (fqConstructorText == (entity.psiElement as? PsiClass)?.qualifiedName) return //fqn совпадает, всё верно, выходим
+            val psiClass = JavaPsiFacade.getInstance(holder.project)
+                .findClass(fqConstructorText, GlobalSearchScope.allScope(holder.project))
 
-                if (entity.name == fqConstructorText) { //Совпадает только с именем сущности
-                    holder.registerProblem(
-                        fqConstructor,
-                        JpaBundle.message("esprito.jpa.inspection.constructor.not.qualified"),
-                        ProblemHighlightType.WARNING
-                    )
+            if (psiClass != null) return
 
-                    return
-                }
+            if (fqConstructor.identifierList.size == 1) {
+                holder.registerProblem(
+                    fqConstructor,
+                    JpaBundle.message("esprito.jpa.inspection.constructor.not.qualified"),
+                    ProblemHighlightType.WARNING
+                )
+            } else {
+                holder.registerProblem(
+                    fqConstructor,
+                    JpaBundle.message("esprito.jpa.inspection.constructor.entity.not.found", fqConstructorText),
+                    ProblemHighlightType.WARNING
+                )
             }
-
-            //не было совпадений
-            holder.registerProblem(
-                fqConstructor,
-                JpaBundle.message("esprito.jpa.inspection.constructor.entity.not.found", fqConstructorText),
-                ProblemHighlightType.WARNING
-            )
-
         }
     }
 
