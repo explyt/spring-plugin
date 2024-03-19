4da2b58d036a2f21f2e3f5508a0916f0217dea34
Yuriy Burka <bu@esprito.com>
2024-03-19 16:35:15 +0000

    #208 - cache annotations on interface
    MR: development/esprito-studio!193

---
+++ modules/spring-core/src/main/kotlin/com/esprito/spring/core/inspections/SpringCacheAnnotationsOnInterfaceInspection.kt
@@ -0,0 +1,78 @@
+package com.esprito.spring.core.inspections
+
+import com.esprito.spring.core.SpringCoreBundle
+import com.esprito.spring.core.SpringCoreClasses.ANNOTATIONS_CACHE
+import com.esprito.util.EspritoPsiUtil.isEqualOrInheritor
+import com.esprito.util.EspritoPsiUtil.isMetaAnnotatedBy
+import com.intellij.codeInspection.AbstractBaseUastLocalInspectionTool
+import com.intellij.codeInspection.InspectionManager
+import com.intellij.codeInspection.ProblemDescriptor
+import com.intellij.codeInspection.ProblemHighlightType
+import com.intellij.psi.PsiClass
+import com.intellij.psi.PsiNameIdentifierOwner
+import org.jetbrains.uast.UClass
+import org.jetbrains.uast.UMethod
+import org.jetbrains.uast.getContainingUClass
+
+class SpringCacheAnnotationsOnInterfaceInspection : AbstractBaseUastLocalInspectionTool() {
+
+    override fun checkMethod(
+        uMethod: UMethod,
+        manager: InspectionManager,
+        isOnTheFly: Boolean
+    ): Array<ProblemDescriptor>? {
+        val uClass = uMethod.getContainingUClass() ?: return null
+        if (!uClass.isInterface) return null
+        val psiClass = uClass.javaPsi
+        if (isImplicitlySubclassed(psiClass)) return null
+        val psiMethod = uMethod.javaPsi
+
+        if (!psiMethod.isMetaAnnotatedBy(ANNOTATIONS_CACHE)) return null
+        val identifyingElement = (uMethod.sourcePsi as? PsiNameIdentifierOwner)?.identifyingElement ?: return null
+
+        return arrayOf(
+            manager.createProblemDescriptor(
+                identifyingElement,
+                SpringCoreBundle.message("esprito.spring.inspection.cache.interface.method"),
+                isOnTheFly,
+                emptyArray(),
+                ProblemHighlightType.GENERIC_ERROR_OR_WARNING
+            )
+        )
+    }
+
+    override fun checkClass(
+        uClass: UClass,
+        manager: InspectionManager,
+        isOnTheFly: Boolean
+    ): Array<ProblemDescriptor>? {
+        if (uClass.isAnnotationType || !uClass.isInterface) return null
+        val psiClass = uClass.javaPsi
+        if (isImplicitlySubclassed(psiClass)) return null
+
+        if (!psiClass.isMetaAnnotatedBy(ANNOTATIONS_CACHE)) return null
+        val identifyingElement = (uClass.sourcePsi as? PsiNameIdentifierOwner)?.identifyingElement ?: return null
+
+        return arrayOf(
+            manager.createProblemDescriptor(
+                identifyingElement,
+                SpringCoreBundle.message("esprito.spring.inspection.cache.interface.class"),
+                isOnTheFly,
+                emptyArray(),
+                ProblemHighlightType.GENERIC_ERROR_OR_WARNING
+            )
+        )
+    }
+
+    private fun isImplicitlySubclassed(psiClass: PsiClass): Boolean {
+
+        return psiClass.isEqualOrInheritor("org.springframework.data.repository.Repository")
+                || psiClass.isMetaAnnotatedBy(
+            listOf(
+                "org.springframework.cloud.netflix.feign.FeignClient",
+                "org.springframework.cloud.openfeign.FeignClient"
+            )
+        )
+    }
+
+}
---
+++ modules/spring-core/src/test/kotlin/com/esprito/spring/core/inspections/java/SpringCacheAnnotationsOnInterfaceInspectionTest.kt
@@ -0,0 +1,15 @@
+package com.esprito.spring.core.inspections.java
+
+import com.esprito.spring.core.inspections.SpringCacheAnnotationsOnInterfaceInspection
+import com.esprito.spring.test.EspritoInspectionJavaTestCase
+import com.esprito.spring.test.TestLibrary
+import org.jetbrains.kotlin.test.TestMetadata
+
+class SpringCacheAnnotationsOnInterfaceInspectionTest : EspritoInspectionJavaTestCase() {
+
+    override val libraries: Array<TestLibrary> = arrayOf(TestLibrary.springBootAutoConfigure_3_1_1)
+
+    @TestMetadata("cacheOnInterface")
+    fun testCacheOnInterface() =
+        doTest(SpringCacheAnnotationsOnInterfaceInspection())
+}
