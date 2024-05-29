c89d12b95aad92d33a9b6dc0430dedd0da1e55e7
Grigorij Myasoedov <gm@esprito.com>
2024-05-29 14:27:02 +0000

    Feature/266 inspection kotlin internal modifier mangling
    MR: development/esprito-studio!251

---
+++ modules/spring-core/src/test/kotlin/com/esprito/spring/core/inspections/kotlin/SpringKotlinInternalBeanInspectionTest.kt
@@ -0,0 +1,43 @@
+package com.esprito.spring.core.inspections.kotlin
+
+import com.esprito.spring.core.SpringCoreClasses
+import com.esprito.spring.core.inspections.SpringKotlinInternalBeanInspection
+import com.esprito.spring.test.EspritoInspectionKotlinTestCase
+import com.esprito.spring.test.TestLibrary
+
+class SpringKotlinInternalBeanInspectionTest : EspritoInspectionKotlinTestCase() {
+    override val libraries: Array<TestLibrary> = arrayOf(TestLibrary.springContext_6_0_7)
+
+    override fun setUp() {
+        super.setUp()
+        myFixture.enableInspections(SpringKotlinInternalBeanInspection::class.java)
+    }
+
+    fun testSimpleBeanWithoutInternal() {
+        myFixture.configureByText(
+            "SpringBean.kt",
+            """
+            @${SpringCoreClasses.COMPONENT}     
+            class SpringBean {
+                @${SpringCoreClasses.BEAN}
+                fun service() {}
+            }
+            """.trimIndent()
+        )
+        myFixture.testHighlighting("SpringBean.kt")
+    }
+
+    fun testInternalBean() {
+        myFixture.configureByText(
+            "SpringBean.kt",
+            """
+            @${SpringCoreClasses.COMPONENT}     
+            class SpringBean {
+                @${SpringCoreClasses.BEAN}
+                internal fun <warning>service</warning>() {}
+            }
+            """.trimIndent()
+        )
+        myFixture.testHighlighting("SpringBean.kt")
+    }
+}
