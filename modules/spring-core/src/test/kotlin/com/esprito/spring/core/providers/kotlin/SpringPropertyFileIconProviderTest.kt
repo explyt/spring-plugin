75f5fb6e228430de418014732faf3d86579d06f5
Olga Kolesnikova <ok@esprito.com>
2024-09-18 08:28:07 +0000

    add icon to property file
    MR: development/esprito-studio!315

---
+++ modules/spring-core/src/test/kotlin/com/esprito/spring/core/providers/kotlin/SpringPropertyFileIconProviderTest.kt
@@ -0,0 +1,60 @@
+package com.esprito.spring.core.providers.kotlin
+
+import com.esprito.spring.core.SpringIcons
+import com.esprito.spring.core.providers.SpringPropertyFileIconProvider
+import com.esprito.spring.test.EspritoKotlinLightTestCase
+import com.esprito.spring.test.TestLibrary
+import com.intellij.psi.PsiFile
+import org.jetbrains.yaml.psi.YAMLFile
+
+class SpringPropertyFileIconProviderTest : EspritoKotlinLightTestCase() {
+
+    private lateinit var iconProvider: SpringPropertyFileIconProvider
+
+    override fun setUp() {
+        super.setUp()
+        iconProvider = SpringPropertyFileIconProvider()
+    }
+
+    override val libraries: Array<TestLibrary> = arrayOf(
+        TestLibrary.springContext_6_0_7,
+        TestLibrary.springTest_6_0_7,
+        TestLibrary.springBootAutoConfigure_3_1_1
+    )
+
+    fun testReturnIconInPropertyYaml() {
+        val yamlFile = myFixture.configureByText("application-test.yaml", "").virtualFile
+        val psiFile = myFixture.psiManager.findFile(yamlFile) as YAMLFile
+
+        val icon = iconProvider.getIcon(psiFile, 0)
+
+        assertNotNull(icon)
+        assertEquals(icon, SpringIcons.SpringSetting)
+    }
+
+    fun testNotReturnIconInPropertyYaml() {
+        val yamlFile = myFixture.configureByText("other-application.yaml", "").virtualFile
+        val psiFile = myFixture.psiManager.findFile(yamlFile) as YAMLFile
+        val icon = iconProvider.getIcon(psiFile, 0)
+
+        assertNull(icon)
+    }
+
+    fun testReturnIconInPropertyProperty() {
+        val file = myFixture.configureByText("application-test.properties", "").virtualFile
+        val propertiesFile = myFixture.psiManager.findFile(file)
+
+        val icon = iconProvider.getIcon(propertiesFile as PsiFile, 0)
+
+        assertNotNull(icon)
+        assertEquals(icon, SpringIcons.SpringSetting)
+    }
+
+    fun testNotReturnIconInPropertyProperty() {
+        val file = myFixture.configureByText("other-application.properties", "").virtualFile
+        val propertiesFile = myFixture.psiManager.findFile(file)
+        val icon = iconProvider.getIcon(propertiesFile as PsiFile, 0)
+
+        assertNull(icon)
+    }
+}
