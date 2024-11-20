d4df9beea227614c7bbae3a77ebf8fc07bddaee3
Ilya Muromtsev <im@explyt.com>
2024-11-21 01:57:17 +0300

    refactoring: remove unused

--- modules/jpa/src/main/kotlin/com/explyt/jpa/JpaBundle.kt
+++ modules/jpa/src/main/kotlin/com/explyt/jpa/JpaBundle.kt
@@ -19,30 +19,13 @@ package com.explyt.jpa
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.Nls
-import org.jetbrains.annotations.NonNls
 import org.jetbrains.annotations.PropertyKey
-import java.util.function.Supplier
 
-class JpaBundle : DynamicBundle(BUNDLE) {
-    companion object {
-        @NonNls
-        private const val BUNDLE = "messages.JpaBundle"
-        @JvmStatic
-        private val INSTANCE: JpaBundle = JpaBundle()
+private const val BUNDLE = "messages.JpaBundle"
 
-        @Nls
-        fun message(
-            key: @PropertyKey(resourceBundle = BUNDLE) String,
-            vararg params: Any
-        ): String {
-            return INSTANCE.messageOrNull(key, *params) ?: ""
-        }
+object JpaBundle : DynamicBundle(BUNDLE) {
 
-        fun messagePointer(
-            key: @PropertyKey(resourceBundle = BUNDLE) String,
-            vararg params: Any
-        ): Supplier<String> {
-            return INSTANCE.getLazyMessage(key, *params)
-        }
-    }
+    @Nls
+    fun message(key: @PropertyKey(resourceBundle = BUNDLE) String, vararg params: Any) =
+        messageOrNull(key, *params) ?: ""
 }
