619b90fdbd20c15b35b1b0b2a437c6ef2f5fe4e9
Evgeny Kharitonov <mors741@yandex.ru>
2025-07-07 14:19:24 +0300

    fix typo in same class inspection (#42)

--- modules/spring-core/src/test/kotlin/com/explyt/spring/core/inspections/kotlin/CallBeanMethodFromSameClassInspectionTest.kt
+++ modules/spring-core/src/test/kotlin/com/explyt/spring/core/inspections/kotlin/CallBeanMethodFromSameClassInspectionTest.kt
@@ -15,101 +15,102 @@
  * Unauthorized use of this code constitutes a violation of intellectual property rights and may result in legal action.
  */
 
-package com.explyt.spring.core.inspections.java
+package com.explyt.spring.core.inspections.kotlin
 
 import com.explyt.spring.core.SpringCoreClasses
-import com.explyt.spring.core.inspections.CallBeanMethodFromSomeClassInspection
-import com.explyt.spring.test.ExplytInspectionJavaTestCase
+import com.explyt.spring.core.inspections.CallBeanMethodFromSameClassInspection
+import com.explyt.spring.test.ExplytInspectionKotlinTestCase
 import com.explyt.spring.test.TestLibrary
 
 
-class CallBeanMethodFromSomeClassInspectionTest : ExplytInspectionJavaTestCase() {
+class CallBeanMethodFromSameClassInspectionTest : ExplytInspectionKotlinTestCase() {
     override val libraries: Array<TestLibrary> = arrayOf(TestLibrary.springContext_6_0_7)
 
     override fun setUp() {
         super.setUp()
-        myFixture.enableInspections(CallBeanMethodFromSomeClassInspection::class.java)
+        myFixture.enableInspections(CallBeanMethodFromSameClassInspection::class.java)
     }
 
     fun testValidSimpleMethodCall() {
         myFixture.configureByText(
-            "SpringComponent.java",
+            "SpringComponent.kt",
             """
             @${SpringCoreClasses.COMPONENT}            
-            public class SpringComponent {
-                public String method() {
-		            return methodSimple(); 
+            class SpringComponent {
+                fun method():String {
+		            return methodSimple()
 	            }
                 	            
-	            public String methodSimple() {
-		            return "simple";
+	            fun methodSimple():String {
+		            return "simple"
 	            }
             }
             """.trimIndent()
         )
-        myFixture.testHighlighting("SpringComponent.java")
+        myFixture.testHighlighting("SpringComponent.kt")
     }
 
     fun testCacheableMethodCall() {
         myFixture.configureByText(
-            "SpringComponent.java",
+            "SpringComponent.kt",
             """
             @${SpringCoreClasses.COMPONENT}            
-            public class SpringComponent {
-                public String method() {
-		            return <warning>cacheableMethod()</warning>; 
+            class SpringComponent {
+                fun method():String {
+		            return <warning>cacheableMethod()</warning>
 	            }
 
                 @${SpringCoreClasses.CACHEABLE}	            
-	            public String cacheableMethod() {
-		            return "cacheable";
+	            fun cacheableMethod():String {
+		            return "cacheable"
 	            }
             }
             """.trimIndent()
         )
-        myFixture.testHighlighting("SpringComponent.java")
+        myFixture.testHighlighting("SpringComponent.kt")
     }
 
     fun testCacheableOnClassMethodCall() {
         myFixture.configureByText(
-            "SpringComponent.java",
+            "SpringComponent.kt",
             """
             @${SpringCoreClasses.COMPONENT}
             @${SpringCoreClasses.CACHEABLE}
-            public class SpringComponent {
-                public String method() {
+            class SpringComponent {
+                fun method():String {
 		            return <warning>cacheableMethod()</warning>; 
 	            }
                 	            
-	            public String cacheableMethod() {
+	            fun cacheableMethod():String {
 		            return "cacheable";
 	            }
             }
             """.trimIndent()
         )
-        myFixture.testHighlighting("SpringComponent.java")
+        myFixture.testHighlighting("SpringComponent.kt")
     }
 
     fun testCacheableFromQualifierCall() {
         myFixture.configureByText(
-            "SpringComponent.java",
+            "SpringComponent.kt",
             """
             @${SpringCoreClasses.COMPONENT}            
-            public class SpringComponent {
-                @${SpringCoreClasses.AUTOWIRED}
-                SpringComponent self;
+            class SpringComponent {
             
-                public String method() {
-		            return self.cacheableMethod(); 
+                @${SpringCoreClasses.AUTOWIRED}
+                val self: SpringComponent? = null
+            
+                fun method():String? {
+		            return self?.cacheableMethod()
 	            }
-                	            
-                @${SpringCoreClasses.CACHEABLE}
-	            public String cacheableMethod() {
-		            return "cacheable";
+
+                @${SpringCoreClasses.CACHEABLE}	            
+	            fun cacheableMethod():String {
+		            return "cacheable"
 	            }
             }
             """.trimIndent()
         )
-        myFixture.testHighlighting("SpringComponent.java")
+        myFixture.testHighlighting("SpringComponent.kt")
     }
 }
