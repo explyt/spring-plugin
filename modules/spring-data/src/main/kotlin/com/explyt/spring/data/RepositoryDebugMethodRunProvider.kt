748d8e454a591d6fab67e8a91133b9095a3bfe34
gmyasoedov <gmyasoedov@yandex.ru>
2025-07-20 16:27:53 +0300

    feat: spring debug - run repository
    (cherry picked from commit 32ff7b8dba98ad4e9a3b77ede265b378bd7b0bea)

--- modules/spring-data/src/main/kotlin/com/explyt/spring/data/RepositoryDebugMethodRunProvider.kt
+++ modules/spring-data/src/main/kotlin/com/explyt/spring/data/RepositoryDebugMethodRunProvider.kt
@@ -17,6 +17,7 @@
 
 package com.explyt.spring.data
 
+import com.explyt.spring.core.externalsystem.utils.Constants.SYSTEM_ID
 import com.explyt.spring.core.runconfiguration.SpringToolRunConfigurationsSettingsState
 import com.explyt.spring.core.statistic.StatisticActionId
 import com.explyt.spring.core.statistic.StatisticService
@@ -24,15 +25,22 @@ import com.explyt.spring.core.util.SpringCoreUtil
 import com.explyt.spring.data.util.SpringDataUtil
 import com.intellij.execution.lineMarker.RunLineMarkerContributor
 import com.intellij.icons.AllIcons
+import com.intellij.lang.Language
+import com.intellij.lang.java.JavaLanguage
 import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.externalSystem.service.notification.ExternalSystemNotificationManager
+import com.intellij.openapi.externalSystem.service.notification.NotificationCategory.WARNING
+import com.intellij.openapi.externalSystem.service.notification.NotificationData
+import com.intellij.openapi.externalSystem.service.notification.NotificationSource
 import com.intellij.psi.PsiElement
 import com.intellij.psi.util.InheritanceUtil
 import com.intellij.xdebugger.XDebuggerManager
 import com.intellij.xdebugger.evaluation.EvaluationMode
 import com.intellij.xdebugger.impl.breakpoints.XExpressionImpl
 import com.intellij.xdebugger.impl.evaluate.XDebuggerEvaluationDialog
+import org.jetbrains.kotlin.idea.KotlinLanguage
 import org.jetbrains.uast.UMethod
 import org.jetbrains.uast.getContainingUClass
 import org.jetbrains.uast.getUParentForIdentifier
@@ -48,16 +56,17 @@ class RepositoryDebugMethodRunProvider : RunLineMarkerContributor() {
         val uClass = uMethod.getContainingUClass().takeIf { it?.isInterface == true } ?: return null
         val qualifiedName = uClass.qualifiedName ?: return null
         if (!InheritanceUtil.isInheritor(uClass.javaPsi, SpringDataClasses.REPOSITORY)) return null
-
+        val language = uClass.javaPsi.language
+            .takeIf { it == JavaLanguage.INSTANCE || it == KotlinLanguage.INSTANCE} ?: return null
         return Info(
             AllIcons.RunConfigurations.TestState.Run,
-            arrayOf(EvaluateInDebugAction(qualifiedName, uMethod.name)),
+            arrayOf(EvaluateInDebugAction(qualifiedName, uMethod.name, language)),
             { "Explyt: Evaluate in Debug" }
         )
     }
 }
 
-private class EvaluateInDebugAction(val qualifiedName: String, val methodName: String)
+private class EvaluateInDebugAction(val qualifiedName: String, val methodName: String, val language: Language)
     : AnAction({ "Explyt: Evaluate in Debug" }, AllIcons.RunConfigurations.TestState.Run) {
     override fun getActionUpdateThread(): ActionUpdateThread {
         return ActionUpdateThread.EDT
@@ -65,14 +74,21 @@ private class EvaluateInDebugAction(val qualifiedName: String, val methodName: S
 
     override fun actionPerformed(e: AnActionEvent) {
         val project = e.project ?: return
+        if (!SpringCoreUtil.isExplytDebug(project)) return
+        if (XDebuggerManager.getInstance(project).currentSession?.currentStackFrame == null) {
+            val notification = NotificationData("", "No active stopped break point", WARNING, NotificationSource.TASK_EXECUTION)
+            notification.isBalloonNotification = true
+            ExternalSystemNotificationManager.getInstance(project).showNotification(SYSTEM_ID, notification)
+            return
+        }
+        val className = if (language == JavaLanguage.INSTANCE) "$qualifiedName.class" else "$qualifiedName::class.java"
         val fromText = XExpressionImpl.fromText(
-            "com.explyt.spring.boot.bean.reader.ExplytContext.context.getBean($qualifiedName.class).$methodName()", EvaluationMode.EXPRESSION)
+            "explyt.Explyt.context.getBean($className).$methodName()", EvaluationMode.EXPRESSION)
         StatisticService.getInstance().addActionUsage(StatisticActionId.GUTTER_DEBUG_DATA_METHOD_EVALUATE)
 
         val debugSession = XDebuggerManager.getInstance(project).currentSession ?: return
         val editorsProvider = debugSession.debugProcess.editorsProvider
-        val dialog = XDebuggerEvaluationDialog(debugSession, editorsProvider, fromText, null, true)
-        dialog.show()
+        XDebuggerEvaluationDialog(debugSession, editorsProvider, fromText, null, true).show()
     }
 
 }
