1a7d9f78be5bfa7dd1ee88e902a478c75c4d980f
gmyasoedov <gmyasoedov@yandex.ru>
2025-07-25 20:30:45 +0300

    feat: spring debug - support external system

--- modules/spring-core/src/main/kotlin/com/explyt/spring/core/debug/SpringDebuggerRunConfigurationExtension.kt
+++ modules/spring-core/src/main/kotlin/com/explyt/spring/core/debug/SpringDebuggerRunConfigurationExtension.kt
@@ -34,12 +34,16 @@ import com.intellij.execution.configurations.RunnerSettings
 import com.intellij.java.library.JavaLibraryUtil
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.application.runReadAction
+import com.intellij.openapi.externalSystem.service.execution.ExternalSystemRunConfiguration
 import com.intellij.openapi.module.Module
+import com.intellij.openapi.module.ModuleManager
+import com.intellij.openapi.module.ModuleUtilCore
 import com.intellij.openapi.roots.DependencyScope
 import com.intellij.openapi.roots.ModuleRootModificationUtil
 import com.intellij.openapi.vfs.LocalFileSystem
 import com.intellij.openapi.vfs.VfsUtil
 import kotlin.io.path.Path
+
 
 const val EXPLYT_SPRING_HOLDER_MARKER_CLASS = "com.explyt.spring.boot.bean.reader.ContextHolder"
 
@@ -51,9 +55,14 @@ class SpringDebuggerRunConfigurationExtension : RunConfigurationExtension() {
 
         val module = getModule(configuration)?.takeIf { isSpringModule(it) } ?: return
         if (javaParameters.vmParametersList.parametersString.contains(EXPLYT_AGENT_JAR)) return
-        val javaAgentEscaping = NativeBootUtils.getJavaAgentParam()
-        javaParameters.vmParametersList.add(javaAgentEscaping)
-        javaParameters.vmParametersList.addProperty(DEBUG_PROGRAM_PARAM, getConfigurationId(configuration))
+
+        if (configuration is ExternalSystemRunConfiguration) {
+            configuration.settings.env.put(DEBUG_PROGRAM_PARAM, getConfigurationId(configuration))
+        } else {
+            val javaAgentEscaping = NativeBootUtils.getJavaAgentParam()
+            javaParameters.vmParametersList.add(javaAgentEscaping)
+            javaParameters.vmParametersList.addProperty(DEBUG_PROGRAM_PARAM, getConfigurationId(configuration))
+        }
 
         ApplicationManager.getApplication().executeOnPooledThread { addExplytContextLibrary(module) }
     }
@@ -62,17 +71,36 @@ class SpringDebuggerRunConfigurationExtension : RunConfigurationExtension() {
         if (!SpringToolRunConfigurationsSettingsState.getInstance().isDebugMode) return false
         return configuration is ApplicationConfiguration
                 || configuration is JavaRunConfigurationBase
+                || configuration is ExternalSystemRunConfiguration
     }
 
-    private fun getModule(runConfiguration: RunConfigurationBase<*>): Module? {
+    private fun getModule(runConfiguration: RunConfigurationBase<*>): ModuleHolder? {
         return when (runConfiguration) {
-            is ApplicationConfiguration -> runConfiguration.modules.firstOrNull()
-            is ModuleBasedConfiguration<*, *> -> runConfiguration.modules.firstOrNull()
+            is ApplicationConfiguration -> runConfiguration.modules.map { ModuleHolder(it) }.firstOrNull()
+            is ModuleBasedConfiguration<*, *> -> runConfiguration.modules.map { ModuleHolder(it) }.firstOrNull()
+            is ExternalSystemRunConfiguration -> findModuleForExternalSystem(runConfiguration)
             else -> null
         }
     }
 
-    private fun isSpringModule(module: Module): Boolean = SpringCoreUtil.isSpringModule(module)
+    private fun findModuleForExternalSystem(configuration: ExternalSystemRunConfiguration): ModuleHolder? {
+        val externalProjectPath = configuration.settings.externalProjectPath
+        val file = VfsUtil.findFile(Path(externalProjectPath), false) ?: return null
+        val module = ModuleUtilCore.findModuleForFile(file, configuration.project) ?: return null
+        val modules = ModuleManager.getInstance(configuration.project).modules
+        val subModules = modules.filter { it.name.startsWith(module.name + ".") }
+            .takeIf { it.isNotEmpty() } ?: return ModuleHolder(module)
+        val mainModule = subModules.find { it.name == module.name + ".main" } ?: return ModuleHolder(module)
+        val testModule = subModules.find { it.name == module.name + ".test" }
+        return ModuleHolder(mainModule, testModule)
+    }
+
+    private fun isSpringModule(holder: ModuleHolder): Boolean = SpringCoreUtil.isSpringModule(holder.mainModule)
+
+    private fun addExplytContextLibrary(holder: ModuleHolder) {
+        addExplytContextLibrary(holder.mainModule)
+        holder.testModule?.let { addExplytContextLibrary(it) }
+    }
 
     private fun addExplytContextLibrary(module: Module) {
         val libraryRoot = Path(NativeBootUtils.getContextLibPath())
@@ -91,3 +119,5 @@ class SpringDebuggerRunConfigurationExtension : RunConfigurationExtension() {
         )
     }
 }
+
+private data class ModuleHolder(val mainModule: Module, val testModule: Module? = null)
