f756609186d9c0fcec9cdc2342f7d52217f72fdf
gmyasoedov <gmyasoedov@yandex.ru>
2025-08-07 16:34:21 +0300

    chore: spring ai
    - convert property
    - convert dto

---
+++ modules/spring-ai/src/main/kotlin/com/explyt/spring/ai/action/ConvertDtoToEntityAction.kt
@@ -0,0 +1,93 @@
+/*
+ * Copyright © 2025 Explyt Ltd
+ *
+ * All rights reserved.
+ *
+ * This code and software are the property of Explyt Ltd and are protected by copyright and other intellectual property laws.
+ *
+ * You may use this code under the terms of the Explyt Source License Version 1.0 ("License"), if you accept its terms and conditions.
+ *
+ * By installing, downloading, accessing, using, or distributing this code, you agree to the terms and conditions of the License.
+ * If you do not agree to such terms and conditions, you must cease using this code and immediately delete all copies of it.
+ *
+ * You may obtain a copy of the License at: https://github.com/explyt/spring-plugin/blob/main/EXPLYT-SOURCE-LICENSE.md
+ *
+ * Unauthorized use of this code constitutes a violation of intellectual property rights and may result in legal action.
+ */
+
+package com.explyt.spring.ai.action
+
+import com.explyt.spring.ai.SpringAiBundle
+import com.explyt.util.ExplytPsiUtil.isMetaAnnotatedBy
+import com.intellij.openapi.actionSystem.ActionUpdateThread
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.project.Project
+import org.jetbrains.kotlin.idea.core.util.toPsiFile
+import org.jetbrains.uast.UFile
+import org.jetbrains.uast.toUElement
+
+private val JPA_ANNOTATIONS = listOf("javax.persistence.Entity", "jakarta.persistence.Entity")
+
+class ConvertDtoToEntityAction : AnAction(SpringAiBundle.message("explyt.spring.ai.action.dto.to.jpa")) {
+    override fun update(e: AnActionEvent) {
+        val project: Project = e.project ?: return
+        val virtualFiles = e.getData(CommonDataKeys.VIRTUAL_FILE_ARRAY)
+
+        val uClasses = virtualFiles
+            ?.mapNotNull { it.toPsiFile(project)?.toUElement() as UFile }
+            ?.flatMap { it.classes } ?: emptyList()
+
+        val enabled = uClasses.isNotEmpty() && uClasses.none { it.javaPsi.isMetaAnnotatedBy(JPA_ANNOTATIONS) }
+
+        e.presentation.isEnabledAndVisible = enabled
+    }
+
+    override fun getActionUpdateThread(): ActionUpdateThread = ActionUpdateThread.BGT
+
+    override fun actionPerformed(e: AnActionEvent) {
+        val project: Project = e.project ?: return
+
+        val dtoPsiClasses = e.getData(CommonDataKeys.VIRTUAL_FILE_ARRAY)
+            ?.mapNotNull { it.toPsiFile(project)?.toUElement() as? UFile }
+            ?.flatMap { it.classes }
+            ?.filter { !it.javaPsi.isMetaAnnotatedBy(JPA_ANNOTATIONS) } ?: return
+
+        val dtoNames = dtoPsiClasses.map { it.javaPsi.name }
+
+        val prompt = SpringAiBundle.message("action.prompt.convert.dto", dtoNames)
+        //service.sendPromptWithClasses(prompt, dtoPsiClasses)
+    }
+}
+
+class ConvertEntityToDtoAction : AnAction(SpringAiBundle.message("explyt.spring.ai.action.jpa.to.dto")) {
+    override fun update(e: AnActionEvent) {
+        val project: Project = e.project ?: return
+        val virtualFiles = e.getData(CommonDataKeys.VIRTUAL_FILE_ARRAY)
+
+        val uClasses = virtualFiles
+            ?.mapNotNull { it.toPsiFile(project)?.toUElement() as UFile }
+            ?.flatMap { it.classes } ?: emptyList()
+
+        val enabled = uClasses.isNotEmpty() && uClasses.any { it.javaPsi.isMetaAnnotatedBy(JPA_ANNOTATIONS) }
+
+        e.presentation.isEnabledAndVisible = enabled
+    }
+
+    override fun getActionUpdateThread(): ActionUpdateThread = ActionUpdateThread.BGT
+
+    override fun actionPerformed(e: AnActionEvent) {
+        val project: Project = e.project ?: return
+
+        val dtoPsiClasses = e.getData(CommonDataKeys.VIRTUAL_FILE_ARRAY)
+            ?.mapNotNull { it.toPsiFile(project)?.toUElement() as? UFile }
+            ?.flatMap { it.classes }
+            ?.filter { it.javaPsi.isMetaAnnotatedBy(JPA_ANNOTATIONS) } ?: return
+
+        val dtoNames = dtoPsiClasses.map { it.javaPsi.name }
+
+        val prompt = SpringAiBundle.message("action.prompt.convert.entity", dtoNames)
+        //service.sendPromptWithClasses(prompt, dtoPsiClasses)
+    }
+}
