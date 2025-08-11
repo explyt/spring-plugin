38d8f91e20273eb50ebcc61427fb492357e7a5a6
gmyasoedov <gmyasoedov@yandex.ru>
2025-08-11 10:31:40 +0300

    fix: property icon in marked property folder
    (cherry picked from commit 80ccc815924452f9fc27b9f0b8bf50b882b4a98c)

--- modules/spring-core/src/main/kotlin/com/explyt/spring/core/settings/SpringPropertyFolderState.kt
+++ modules/spring-core/src/main/kotlin/com/explyt/spring/core/settings/SpringPropertyFolderState.kt
@@ -20,6 +20,7 @@ package com.explyt.spring.core.settings
 import com.intellij.openapi.components.*
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.psi.PsiFile
 
 @Service(Service.Level.PROJECT)
 @State(name = "ExplytSpringPropertyFolderState", storages = [Storage("explyt-spring-property-folder.xml")])
@@ -34,6 +35,11 @@ class SpringPropertyFolderState : SimplePersistentStateComponent<PropertyFolders
             val folders = getInstance(project).state.folders
                 .takeIf { it.isNotEmpty() } ?: return false
             return folders.any { canonicalPath.startsWith(it) }
+        }
+
+        fun isUserPropertyFolder(currentPsiFile: PsiFile): Boolean {
+            val virtualFile = currentPsiFile.virtualFile?.parent ?: return false
+            return isUserPropertyFolder(currentPsiFile.project, virtualFile)
         }
 
         fun addUserPropertyFolder(project: Project, virtualFile: VirtualFile) {
