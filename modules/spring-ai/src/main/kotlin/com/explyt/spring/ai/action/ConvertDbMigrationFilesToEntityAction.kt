dbb2bf3070d56cc2ebeedf7a765e7ddc74db2c66
gmyasoedov <gmyasoedov@yandex.ru>
2025-09-13 21:58:50 +0300

    feat: registrar line marker
    cleanup code
    
    (cherry picked from commit 20aeffa05a3c31a7f4c4ca2cf017d1c2acd2e3d1)

--- modules/spring-ai/src/main/kotlin/com/explyt/spring/ai/action/ConvertDbMigrationFilesToEntityAction.kt
+++ modules/spring-ai/src/main/kotlin/com/explyt/spring/ai/action/ConvertDbMigrationFilesToEntityAction.kt
@@ -19,12 +19,13 @@ package com.explyt.spring.ai.action
 
 import com.explyt.spring.ai.SpringAiBundle
 import com.explyt.spring.ai.service.AiPluginService
-import com.explyt.spring.ai.service.AiUtils
 import com.explyt.spring.core.util.ActionUtil
+import com.explyt.util.ModuleUtil
 import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.module.ModuleUtilCore
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.openapi.vfs.isFile
 import com.intellij.openapi.vfs.readText
@@ -54,10 +55,11 @@ class ConvertDbMigrationFilesToEntityAction : AnAction(SpringAiBundle.message("e
         val file = files.firstOrNull() ?: return
         val isJakarta = JavaPsiFacade.getInstance(project).findClass(JAKARTA_ENTITY, project.allScope()) != null
         val entity = if (isJakarta) JAKARTA_ENTITY else JPA_ENTITY
-        val languageSentence = AiUtils.getLanguageSentence(project, file)
+        val language = ModuleUtilCore.findModuleForFile(file, project)
+            ?.let { if (ModuleUtil.isKotlinModule(it)) "Kotlin" else "Java" } ?: "Java"
         val prompt =
             "Convert DB migration - liquibase/flyway to the $entity. From file - '${file.name}'. Create new classes. " +
-                    "Save result files to the corresponding directory. $languageSentence"
+                    "Save result files to the corresponding directory. $language language."
         AiPluginService.getInstance(project).performPrompt(prompt, files)
     }
 
