1aba8a8b70262e143728cd817cb7245f49a6f4d8
gmyasoedov <gmyasoedov@yandex.ru>
2025-09-17 10:42:10 +0300

    feat: jdbc client - tests #68
    (cherry picked from commit 8350eab65c8c85bd7302ac6a22f253e4bffe970e)

--- modules/spring-data/src/main/kotlin/com/explyt/spring/data/inspection/SpringJdbcClientParamInspection.kt
+++ modules/spring-data/src/main/kotlin/com/explyt/spring/data/inspection/SpringJdbcClientParamInspection.kt
@@ -127,7 +127,9 @@ private class UastReferenceVisitor(
                     val argumentForParameter = it.getArgumentForParameter(0) ?: return@forEach
                     val sourcePsi = argumentForParameter.sourcePsi ?: return@forEach
                     val paramName = argumentForParameter.evaluateString() ?: return@forEach
-                    if (!clientCallData.sqlString.contains(":$paramName")) {
+                    if (!clientCallData.sqlString.contains(":$paramName ")
+                        && !clientCallData.sqlString.endsWith(":$paramName")
+                    ) {
                         val message = SpringDataBundle
                             .message("explyt.spring.jdbc.client.inspection.param.not.found", paramName)
                         holder.registerProblem(sourcePsi, message, ProblemHighlightType.WARNING)
