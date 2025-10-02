8bf8a840007912ec56df9cac71ab3b6f727c0c6a
gmyasoedov <gmyasoedov@yandex.ru>
2025-10-02 11:30:06 +0300

    fix: load class for debugger + transaction info

--- modules/spring-core/src/main/kotlin/com/explyt/spring/core/externalsystem/process/SpringBootOpenProjectProvider.kt
+++ modules/spring-core/src/main/kotlin/com/explyt/spring/core/externalsystem/process/SpringBootOpenProjectProvider.kt
@@ -91,8 +91,6 @@ class SpringBootOpenProjectProvider : AbstractOpenProjectProvider() {
         ExternalProjectsManagerImpl.getInstance(project).runWhenInitialized {
             val importSpecBuilder = ImportSpecBuilder(project, SYSTEM_ID)
                 .projectResolverPolicy(DebugProjectResolverPolicy(rawBeanData))
-                .withActivateToolWindowOnStart(false)
-                .withActivateToolWindowOnFailure(false)
             ExternalSystemUtil.refreshProject(canonicalPath, importSpecBuilder)
         }
     }
