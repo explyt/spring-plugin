417c428f09fc46a78f620135606d7caae54a513b
gmyasoedov <gmyasoedov@yandex.ru>
2025-10-10 17:39:44 +0300

    fix: chat action (internal) for 251/252 only

--- modules/spring-ai/src/main/kotlin/com/explyt/spring/ai/service/AiPluginService.kt
+++ modules/spring-ai/src/main/kotlin/com/explyt/spring/ai/service/AiPluginService.kt
@@ -17,7 +17,6 @@
 
 package com.explyt.spring.ai.service
 
-import com.explyt.chat.api.v1.AgentChatApi
 import com.explyt.spring.ai.SpringAiBundle
 import com.explyt.spring.core.runconfiguration.SpringToolRunConfigurationsSettingsState
 import com.intellij.ide.BrowserUtil
@@ -36,6 +35,10 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.text.StringUtil
 import com.intellij.openapi.vfs.VirtualFile
 import java.time.Instant
+import kotlin.reflect.KCallable
+import kotlin.reflect.full.companionObject
+import kotlin.reflect.full.companionObjectInstance
+import kotlin.reflect.full.declaredMembers
 
 private const val GITHUB_WIKI_URL = "https://github.com/explyt/spring-plugin/wiki/Explyt-AI-Actions"
 
@@ -48,7 +51,13 @@ class AiPluginService(private val project: Project) {
 
     fun performPrompt(prompt: String, virtualFiles: List<VirtualFile>) {
         try {
-            AgentChatApi.getInstance(project).createNewChatAndSendRequest(prompt, virtualFiles)
+            val klass = Class.forName("com.explyt.chat.api.v1.AgentChatApi").kotlin
+            val companionObject = klass.companionObject!!
+            val getInstanceMember = companionObject.declaredMembers.first { it.name == "getInstance" }
+            val instanceChat = getInstanceMember.call(klass.companionObjectInstance, project)!!
+            val newChatMember = instanceChat::class.declaredMembers.first { isNewChatMember(it) }
+            newChatMember.call(instanceChat, prompt, virtualFiles.toList(), true)
+            //AgentChatApi.getInstance(project).createNewChatAndSendRequest(prompt, virtualFiles)
         } catch (e: Throwable) {
             logger.warn("Explyt - error run AI from Spring", e)
             Notification(
@@ -61,6 +70,14 @@ class AiPluginService(private val project: Project) {
                 installPlugin(project)
             }).notify(null)
         }
+    }
+
+    private fun isNewChatMember(callable: KCallable<*>): Boolean {
+        if (callable.name != "createNewChatAndSendRequest") return false
+        if (callable.parameters.size < 3) return false
+        if (callable.parameters[1].name != "prompt") return false
+        if (callable.parameters[2].name != "files") return false
+        return true
     }
 
     fun checkAiPlugin(project: Project) {
