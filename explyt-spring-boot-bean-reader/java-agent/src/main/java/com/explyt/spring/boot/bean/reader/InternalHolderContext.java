79c09b3260ee29b0d7643a83be1cf22222a6ff26
gmyasoedov <gmyasoedov@yandex.ru>
2025-11-18 13:49:32 +0300

    feat: Multi context support

--- explyt-spring-boot-bean-reader/java-agent/src/main/java/com/explyt/spring/boot/bean/reader/InternalHolderContext.java
+++ explyt-spring-boot-bean-reader/java-agent/src/main/java/com/explyt/spring/boot/bean/reader/InternalHolderContext.java
@@ -23,13 +23,13 @@ import org.springframework.core.env.ConfigurableEnvironment;
 import tech.ytsaurus.spyt.patch.annotations.AddClass;
 
 import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 @AddClass
 public class InternalHolderContext {
     private static AbstractApplicationContext context;
+
+    public static Map<String, AbstractApplicationContext> contexts = new HashMap<>();
 
     public static AbstractApplicationContext getContext() {
         return context;
@@ -43,6 +43,11 @@ public class InternalHolderContext {
         return context.getBeanFactory();
     }
 
+    public static void addContext(AbstractApplicationContext context) {
+        InternalHolderContext.context = context;
+        contexts.put(context.getId(), context);
+    }
+
     public static String getRawBeanData() {
         if (context == null) return "";
         Class<?> aClass = context.getClass();
@@ -53,10 +58,13 @@ public class InternalHolderContext {
                 .orElse(null);
         if (method == null) return "";
         try {
-            ConfigurableListableBeanFactory beanFactory = context.getBeanFactory();
-            List<String> result = new ArrayList<>(beanFactory.getBeanDefinitionCount());
-            method.invoke(context, beanFactory, result);
+            List<String> result = new ArrayList<>(context.getBeanFactory().getBeanDefinitionCount());
+            for (AbstractApplicationContext ctx : contexts.values()) {
+                method.invoke(ctx, ctx.getBeanFactory(), result);
+            }
             if (result.size() <= 1) return "";
+
+            result = prepareResult(result, contexts);
             StringBuilder builder = new StringBuilder(1000);
             for (String rowBeanInfo : result) {
                 builder.append(";").append(rowBeanInfo);
@@ -65,5 +73,22 @@ public class InternalHolderContext {
         } catch (Exception e) {
             return "error";
         }
+    }
+
+    private static List<String> prepareResult(List<String> result, Map<String, AbstractApplicationContext> contexts) {
+        if (contexts.size() <= 1) return result;
+        LinkedHashSet<String> beans = new LinkedHashSet<>(result.size() / 2);
+        LinkedHashSet<String> aspects = new LinkedHashSet<>();
+        for (String rowBeanData : result) {
+            if (rowBeanData.startsWith(Constants.EXPLYT_BEAN_INFO)) {
+                beans.add(rowBeanData);
+            } else {
+                aspects.add(rowBeanData);
+            }
+        }
+        List<String> totalResult = new ArrayList<>(beans.size() + aspects.size());
+        totalResult.addAll(beans);
+        totalResult.addAll(aspects);
+        return totalResult;
     }
 }
