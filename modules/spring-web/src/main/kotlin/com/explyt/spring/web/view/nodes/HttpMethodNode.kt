ac30be6bc1917c1862da35becf2855ebfb2ce8a5
gmyasoedov <gmyasoedov@yandex.ru>
2025-11-28 11:28:00 +0300

    feat: Endpoints for Message Brokers & Spring Boot

--- modules/spring-web/src/main/kotlin/com/explyt/spring/web/view/nodes/HttpMethodNode.kt
+++ modules/spring-web/src/main/kotlin/com/explyt/spring/web/view/nodes/HttpMethodNode.kt
@@ -18,14 +18,16 @@
 package com.explyt.spring.web.view.nodes
 
 import com.explyt.spring.web.SpringWebIcons
+import com.explyt.spring.web.loader.EndpointType
 import com.explyt.spring.web.view.EndpointElementViewData
 import com.intellij.icons.AllIcons
+import com.intellij.pom.Navigatable
 import com.intellij.ui.treeStructure.CachingSimpleNode
 import com.intellij.ui.treeStructure.SimpleNode
 
 class HttpMethodNode(
     val httpElement: EndpointElementViewData, rootNode: CachingSimpleNode
-) : CachingSimpleNode(rootNode) {
+) : CachingSimpleNode(rootNode), EndpointNavigable {
 
     init {
         when (httpElement.method.uppercase()) {
@@ -38,6 +40,9 @@ class HttpMethodNode(
             "PUT" -> presentation.setIcon(SpringWebIcons.HttpPut)
             "POST" -> presentation.setIcon(SpringWebIcons.HttpPost)
             "TRACE" -> presentation.setIcon(SpringWebIcons.HttpTrace)
+
+            EndpointType.MESSAGE_BROKER.name -> presentation.setIcon(AllIcons.Webreferences.MessageQueue)
+
             else -> presentation.setIcon(AllIcons.General.Web)
         }
     }
@@ -45,4 +50,8 @@ class HttpMethodNode(
     override fun getName() = httpElement.path
 
     override fun buildChildren() = emptyArray<SimpleNode>()
+
+    override fun navigate() {
+        (httpElement.psiPointer.element?.navigationElement as? Navigatable)?.navigate(true)
+    }
 }
