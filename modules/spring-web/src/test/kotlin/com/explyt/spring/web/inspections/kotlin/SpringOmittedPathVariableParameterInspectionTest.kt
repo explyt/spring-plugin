e1565ec5a4d7326582401af242c2924d217b5f5e
gmyasoedov <gmyasoedov@yandex.ru>
2025-12-01 09:55:46 +0300

    fix: Kotlin template string inspection PathVariable #142

--- modules/spring-web/src/test/kotlin/com/explyt/spring/web/inspections/kotlin/SpringOmittedPathVariableParameterInspectionTest.kt
+++ modules/spring-web/src/test/kotlin/com/explyt/spring/web/inspections/kotlin/SpringOmittedPathVariableParameterInspectionTest.kt
@@ -17,15 +17,42 @@
 
 package com.explyt.spring.web.inspections.kotlin
 
+import com.explyt.spring.core.SpringCoreClasses
 import com.explyt.spring.test.ExplytInspectionKotlinTestCase
 import com.explyt.spring.test.TestLibrary
 import com.explyt.spring.web.inspections.SpringOmittedPathVariableParameterInspection
+import org.intellij.lang.annotations.Language
 import org.jetbrains.kotlin.test.TestMetadata
 
 class SpringOmittedPathVariableParameterInspectionTest : ExplytInspectionKotlinTestCase() {
 
-    override val libraries: Array<TestLibrary> = arrayOf(TestLibrary.springWeb_6_0_7)
+    override val libraries: Array<TestLibrary> = arrayOf(
+        TestLibrary.springContext_6_0_7, TestLibrary.springWeb_6_0_7
+    )
+
+    override fun setUp() {
+        super.setUp()
+        myFixture.enableInspections(SpringOmittedPathVariableParameterInspection::class.java)
+    }
 
     @TestMetadata("pathVariableController")
     fun testPathVariableController() = doTest(SpringOmittedPathVariableParameterInspection())
+
+    fun testKotlinStringExpression() {
+        @Language("kotlin") val code = """
+            import org.springframework.web.bind.annotation.PostMapping
+            const val str = "str" 
+            
+            @${SpringCoreClasses.COMPONENT}            
+            class SpringComponent {
+                                 
+                @PostMapping("post1/${'$'}{str}")
+                fun postStr(): String {
+                    return "postStr"
+                }
+            }
+            """.trimIndent()
+        myFixture.configureByText("SpringComponent.kt", code)
+        myFixture.testHighlighting("SpringComponent.kt")
+    }
 }
