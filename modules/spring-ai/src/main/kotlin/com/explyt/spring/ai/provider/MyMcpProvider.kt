74d23107194428d44c073a3c8539ddc7589951c0
gmyasoedov <gmyasoedov@yandex.ru>
2025-12-22 09:59:12 +0300

    feat: MCP Server

---
+++ modules/spring-ai/src/main/kotlin/com/explyt/spring/ai/provider/MyMcpProvider.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright © 2025 Explyt Ltd
+ *
+ * All rights reserved.
+ *
+ * This code and software are the property of Explyt Ltd and are protected by copyright and other intellectual property laws.
+ *
+ * You may use this code under the terms of the Explyt Source License Version 1.0 ("License"), if you accept its terms and conditions.
+ *
+ * By installing, downloading, accessing, using, or distributing this code, you agree to the terms and conditions of the License.
+ * If you do not agree to such terms and conditions, you must cease using this code and immediately delete all copies of it.
+ *
+ * You may obtain a copy of the License at: https://github.com/explyt/spring-plugin/blob/main/EXPLYT-SOURCE-LICENSE.md
+ *
+ * Unauthorized use of this code constitutes a violation of intellectual property rights and may result in legal action.
+ */
+
+package com.explyt.spring.ai.provider
+
+import com.intellij.mcpserver.McpToolset
+import com.intellij.mcpserver.annotations.McpDescription
+import com.intellij.mcpserver.annotations.McpTool
+import com.intellij.openapi.project.getOpenedProjects
+
+
+class MyMcpToolset : McpToolset {
+
+    @McpTool
+    @McpDescription(description = "My Reformats a specified file in the JetBrains IDE.\n        |Use this tool to apply code formatting rules to a file identified by its path.\n  ")
+    suspend fun test(path: String): String {
+        getOpenedProjects()
+
+        // resolveInProject
+        return "hello"
+    }
+}
+
