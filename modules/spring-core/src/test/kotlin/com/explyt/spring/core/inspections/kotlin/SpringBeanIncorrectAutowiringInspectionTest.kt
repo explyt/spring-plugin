c35d270fa047506d225973a7526da636838e9a94
gmyasoedov <gmyasoedov@yandex.ru>
2026-02-23 13:33:27 +0300

    fix: support ResourceLoader #190

--- modules/spring-core/src/test/kotlin/com/explyt/spring/core/inspections/kotlin/SpringBeanIncorrectAutowiringInspectionTest.kt
+++ modules/spring-core/src/test/kotlin/com/explyt/spring/core/inspections/kotlin/SpringBeanIncorrectAutowiringInspectionTest.kt
@@ -17,9 +17,11 @@
 
 package com.explyt.spring.core.inspections.kotlin
 
+import com.explyt.spring.core.SpringCoreClasses
 import com.explyt.spring.core.inspections.SpringBeanIncorrectAutowiringInspection
 import com.explyt.spring.test.ExplytInspectionKotlinTestCase
 import com.explyt.spring.test.TestLibrary
+import org.intellij.lang.annotations.Language
 import org.jetbrains.kotlin.test.TestMetadata
 
 class SpringBeanIncorrectAutowiringInspectionTest : ExplytInspectionKotlinTestCase() {
@@ -30,6 +32,29 @@ class SpringBeanIncorrectAutowiringInspectionTest : ExplytInspectionKotlinTestCa
         TestLibrary.springBootTestAutoConfigure_3_1_1
     )
 
+    override fun setUp() {
+        super.setUp()
+        myFixture.enableInspections(SpringBeanIncorrectAutowiringInspection::class.java)
+    }
+
     @TestMetadata("autowired")
     fun testAutowired() = doTest(SpringBeanIncorrectAutowiringInspection())
+
+    fun testResourceLoader() {
+        @Language("kotlin") val code = """
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.ApplicationContext
+import org.springframework.core.io.ResourceLoader
+import org.springframework.context.support.AbstractApplicationContext
+
+@${SpringCoreClasses.COMPONENT}
+class DemoApplication {
+    @Autowired var resourceLoader: ResourceLoader? = null
+    @Autowired var context: ApplicationContext? = null
+    @Autowired var abstractContext: AbstractApplicationContext? = null
+}
+            """
+        myFixture.configureByText("DemoApplication.kt", code.trimIndent())
+        myFixture.testHighlighting("DemoApplication.kt")
+    }
 }
