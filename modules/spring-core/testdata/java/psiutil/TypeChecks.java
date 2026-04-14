cca24e93ddeb709ae02627b637fe8960fdb6a0aa
Ilya Muromtsev <im@explyt.com>
2026-04-14 20:25:12 +0300

    chore: Add tests with TypeChecks
    (cherry picked from commit d520c150bab1b3544b74a2799af54cf9c6d4ddb4)

---
+++ modules/spring-core/testdata/java/psiutil/TypeChecks.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright © 2025 Explyt Ltd
+ *
+ * All rights reserved.
+ *
+ * This code and software are the property of Explyt Ltd and are protected by copyright and other intellectual property laws.
+ *
+ * You may use this code under the terms of the Explyt Source License Version 1.0 ("License"), if you accept its terms and conditions.
+ *
+ * By installing, downloading, accessing, using, or distributing this code, you agree to the terms and conditions of the License.
+ * If you do not agree to such terms and conditions, you must cease using this code and immediately delete all copies of it.
+ *
+ * You may obtain a copy of the License at: https://github.com/explyt/spring-plugin/blob/main/EXPLYT-SOURCE-LICENSE.md
+ *
+ * Unauthorized use of this code constitutes a violation of intellectual property rights and may result in legal action.
+ */
+
+import java.util.*;
+
+public class TypeChecks {
+    List<String> javaList;
+    Collection<String> javaCollection;
+    Map<String, String> javaMap;
+    Set<String> javaSet;
+    ArrayList<String> javaArrayList;
+    HashMap<String, String> javaHashMap;
+    Optional<String> javaOptional;
+    String javaString;
+    int javaPrimitive;
+}
