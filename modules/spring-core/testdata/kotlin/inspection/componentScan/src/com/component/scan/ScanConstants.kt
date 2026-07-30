265def3fd06c8feca479ab919226ac5b56d093ec
Ilya Muromtsev <im@explyt.com>
2026-07-30 13:11:34 +0300

    fix: Resolve Kotlin const val package names in component-scan annotations (#275)
    (cherry picked from commit 31b55179fa80feb5fc24829267c450b23a1be6f9)

---
+++ modules/spring-core/testdata/kotlin/inspection/componentScan/src/com/component/scan/ScanConstants.kt
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2026 Explyt Ltd
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.component.scan
+
+const val CROSS_FILE_BASE_PACKAGE = "com.component.scan"
+const val CROSS_FILE_INVALID_BASE_PACKAGE = "com.component.invalid"
