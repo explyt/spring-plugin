ec12b5dffcfc6819642631d07424fff298499eec
Ilya Muromtsev <im@explyt.com>
2026-09-16 23:36:15 +0300

    fix: Navigate list elements nested under a map entry (#396) (#399)

--- modules/spring-core/src/test/kotlin/com/explyt/spring/core/reference/kotlin/PropertiesReferenceTest.kt
+++ modules/spring-core/src/test/kotlin/com/explyt/spring/core/reference/kotlin/PropertiesReferenceTest.kt
@@ -167,6 +167,52 @@ main.enum-value-additional=TUE<caret>SDAY
         assertEquals("payloadType", resolveListElementReferenceName())
     }
 
+    fun testRefKeyListElementUnderBracketMapKey() {
+        myFixture.copyFileToProject("BracketMapProperties.kt")
+        myFixture.configureByText(
+            "application.properties",
+            "app.publishers[my.registration].routes[0].payload-<caret>type=my.event"
+        )
+
+        assertEquals("setPayloadType", mapValueReferenceName())
+    }
+
+    fun testRefKeyListElementUnderMapKeyConstructorBoundDataClass() {
+        myFixture.copyFileToProject("ConstructorBoundProperties.kt")
+        myFixture.configureByText(
+            "application.properties",
+            "app.publishers[my.registration].routes[0].payload-<caret>type=my.event"
+        )
+
+        assertEquals("payloadType", mapValueReferenceName())
+    }
+
+    fun testRefKeyListElementUnderMapKeyUnknownMemberIsNotResolved() {
+        myFixture.copyFileToProject("BracketMapProperties.kt")
+        myFixture.configureByText(
+            "application.properties",
+            "app.publishers[my.registration].routes[0].unkn<caret>own=my.event"
+        )
+
+        assertNull(mapValueReferenceName())
+    }
+
+    private fun mapValueReferenceName(): String? = mapValueReference()
+        .multiResolve(true)
+        .singleOrNull()
+        ?.let { (it.element as? ConfigKeyPsiElement)?.name }
+
+    private fun mapValueReference(): PropertiesKeyMapValueReference {
+        val found = file.findReferenceAt(myFixture.caretOffset)
+        val reference = when (found) {
+            is PsiMultiReference -> found.references.filterIsInstance<PropertiesKeyMapValueReference>().lastOrNull()
+            is PropertiesKeyMapValueReference -> found
+            else -> null
+        }
+        assertNotNull("map value reference must be contributed", reference)
+        return reference!!
+    }
+
     private fun resolveListElementReferenceName(): String? {
         val reference = (file.findReferenceAt(myFixture.caretOffset) as? PsiMultiReference)
             ?.references
